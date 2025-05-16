7a594c19c1c1b6d1c9dc8193e32f02bd32887c31
Muzammil Zaidi <muzammil.s3517@gmail.com>
2025-05-16 15:36:58 +0500

    Update Pointers in C.c

--- C/Pointers in C.c
+++ C/Pointers in C.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+#include<stdlib.h>
 
+void update(int *a,int *b) {
+    // Complete this function
+    int sum=*a;
+    int sub=*b;
+    *a=sum+sub;
+    *b=abs(sum-sub);    
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
