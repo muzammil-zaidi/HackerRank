11d42a0bdf82534c26fb088d4cbe3bb12b44b231
Muzammil Zaidi <muzammil.s3517@gmail.com>
2025-05-20 17:26:42 +0500

    Update Sum of Digits of a Five Digit Number.c

--- C/Sum of Digits of a Five Digit Number.c
+++ C/Sum of Digits of a Five Digit Number.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
 
+int main() {
+	
+    int n;
+    scanf("%d", &n);
+    //Complete the code to calculate the sum of the five digits on n.
+    int a,b,c,d,e;
+    a=n%10;
+    b=(n%100)/10;
+    c=(n%1000)/100;
+    d=(n%10000)/1000;
+    e=(n/10000);
+    int sum=0;
+    sum=a+b+c+d+e;
+    printf("%d",sum);
+    return 0;
+}
