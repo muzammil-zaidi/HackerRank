d1e49fe65865828f18ec8ec3bca3347fa37cfa19
Muzammil Zaidi <muzammil.s3517@gmail.com>
2025-05-26 01:59:07 +0500

    Update Small Triangles, Large Triangles.c

--- C/Small Triangles, Large Triangles.c
+++ C/Small Triangles, Large Triangles.c
@@ -1,1 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
+struct triangle
+{
+	int a;
+	int b;
+	int c;
+};
+
+typedef struct triangle triangle;
+double calculate_area(triangle tr) {
+    double p = (tr.a + tr.b + tr.c) / 2.0; // Semi-perimeter
+    return sqrt(p * (p - tr.a) * (p - tr.b) * (p - tr.c)); // Area
+}
+void sort_by_area(triangle* tr, int n) {
+	/**
+	* Sort an array a of the length n
+	*/
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            // Calculate areas of adjacent triangles
+            double area1 = calculate_area(tr[j]);
+            double area2 = calculate_area(tr[j + 1]);
+
+            // Swap if the first triangle has a larger area than the second
+            if (area1 > area2) {
+                triangle temp = tr[j];
+                tr[j] = tr[j + 1];
+                tr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main()
+{
+	int n;
+	scanf("%d", &n);
+	triangle *tr = malloc(n * sizeof(triangle));
+	for (int i = 0; i < n; i++) {
+		scanf("%d%d%d", &tr[i].a, &tr[i].b, &tr[i].c);
+	}
+	sort_by_area(tr, n);
+	for (int i = 0; i < n; i++) {
+		printf("%d %d %d\n", tr[i].a, tr[i].b, tr[i].c);
+	}
+	return 0;
+}
