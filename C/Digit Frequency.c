2825c185f988e4e66b37633156106b957b91f54c
Muzammil Zaidi <muzammil.s3517@gmail.com>
2025-05-28 07:23:25 +0500

    Update Digit Frequency.c

--- C/Digit Frequency.c
+++ C/Digit Frequency.c
@@ -8,7 +8,7 @@ int main() {
 
     for (int i = 0; i < strlen(str); i++) {
         if (str[i] >= '0' && str[i] <= '9') {
-            freq[str[i] - '0']++;  // Convert char to corresponding integer index
+            freq[str[i] - '0']++;
         }
     }
 
