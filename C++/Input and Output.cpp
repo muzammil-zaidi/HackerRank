1bc7ffd9abdf61cc3d94dcadf38697f59267fe9f
Muzammil Zaidi <muzammil.s3517@gmail.com>
2025-06-02 00:22:17 +0500

    Update Input and Output.cpp

--- C++/Input and Output.cpp
+++ C++/Input and Output.cpp
@@ -1,1 +1,19 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
 
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    int x,y,z;
+    int a=0;
+    cin>>x;
+    cin>>y;
+    cin>>z;
+    a=x+y+z;
+    cout<<a;  
+    return 0;
+}
