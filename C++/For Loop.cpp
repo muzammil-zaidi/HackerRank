7bb81c50592e7fdb1fe250dfdf54bfea3568cc10
Muzammil Zaidi <muzammil.s3517@gmail.com>
2025-06-29 15:22:38 +0500

    Update For Loop.cpp

--- C++/For Loop.cpp
+++ C++/For Loop.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    string numbers[] = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+    
+    for (int i = a; i <= b; i++) {
+        if (i >= 1 && i <= 9) {
+            cout << numbers[i] << endl;
+        } else if (i % 2 == 0) {
+            cout << "even" << endl;
+        } else {
+            cout << "odd" << endl;
+        }
+    }
+    
+    return 0;
+}
