d3e63bf5f6412bacc58bfe82be349dc69dcaf29c
Muzammil Zaidi <muzammil.s3517@gmail.com>
2025-07-01 02:38:02 +0500

    Update Functions.cpp

--- C++/Functions.cpp
+++ C++/Functions.cpp
@@ -1,1 +1,30 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
 
+
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int max;
+    
+    if(a>b && a>c && a>d){
+        max=a;
+    }
+    
+    else if(b>a && b>c && b>d){
+        max=b;
+    }
+    
+    else if(c>b && c>a && c>d){
+        max=c;
+    }
+    
+    else{
+        max=d;
+    }
+    
+    cout<<max;
+    
+    return 0;
+}
