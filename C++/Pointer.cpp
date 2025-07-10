6274ec3ba14c564009ec471deb500ed6ce492ccb
Muzammil Zaidi <muzammil.s3517@gmail.com>
2025-07-10 16:29:06 +0500

    Update Pointer.cpp

--- C++/Pointer.cpp
+++ C++/Pointer.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int a, b;
+    cin>>a>>b;
+    int c=0, d=0;
+    
+    c=a+b;
+    d=a-b;
+    
+    if(d<0){
+        d=d*(-1);
+    }
+    
+    cout<<c<<endl;
+    cout<<d;
+
+    return 0;
+}
