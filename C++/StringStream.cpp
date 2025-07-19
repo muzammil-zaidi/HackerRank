fde2f7b87a0a2b6b0313fbe0c7113aa3ac778cef
Muzammil Zaidi <muzammil.s3517@gmail.com>
2025-07-19 17:45:16 +0500

    Update StringStream.cpp

--- C++/StringStream.cpp
+++ C++/StringStream.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <sstream>
 
+using namespace std;
+
+vector<int> parseInts(string str) {
+    vector<int> result;
+    stringstream ss(str);
+    int number;
+    char ch;
+    
+    while (ss >> number) { // Extract integer
+        result.push_back(number);
+        ss >> ch; // Extract comma
+    }
+    
+    return result;
+}
+
+int main() {
+    string str;
+    cin >> str;
+    vector<int> integers = parseInts(str);
+    
+    for (int num : integers) {
+        cout << num << "\n";
+    }
+    
+    return 0;
+}
