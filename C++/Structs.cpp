858720caf985a2891407c3acf1d0b3e5a5688af2
Muzammil Zaidi <muzammil.s3517@gmail.com>
2025-07-22 17:03:14 +0500

    Update Structs.cpp

--- C++/Structs.cpp
+++ C++/Structs.cpp
@@ -1,1 +1,16 @@
+#include<iostream>
+#include<string>
+using namespace std;
 
+int main(){
+    int a,b;
+    string c,d;
+    
+    cin>>a;
+    cin>>c;
+    cin>>d;
+    cin>>b;
+    
+    cout<<a<<" "<<c<<" "<<d<<" "<<b;
+    return 0;
+}
