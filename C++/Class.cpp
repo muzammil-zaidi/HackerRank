1aa7ce0668c0d36460e4661f4d1e5669f9511c72
Muzammil Zaidi <muzammil.s3517@gmail.com>
2025-07-26 02:50:40 +0500

    Update Class.cpp

--- C++/Class.cpp
+++ C++/Class.cpp
@@ -1,1 +1,20 @@
+#include<iostream>
+#include <string>
+using namespace std;
 
+int main(){
+    int a,b;
+    string c,d;
+    
+    cin>>a;
+    cin>>c;
+    cin>>d;
+    cin>>b;
+    
+    cout<<a<<endl;
+    cout<<d<<", "<<c<<endl;
+    cout<<b<<endl<<endl;
+    cout<<a<<","<<c<<","<<d<<","<<b;
+    
+    return 0;
+}
